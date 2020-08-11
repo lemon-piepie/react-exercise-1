a136815c99fa7eb7ef8f61a25f48ae482ecb4a41
lemon-piepie <purplechilde@163.com>
2020-08-11 19:05:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
+import HeadPart from './Components/HeadPart';
+import AboutMe from './Components/AboutMe';
+import Education from './Components/Education';
 
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return(
+    <main className="app">
+      <HeadPart name = "KAMIL 24YO"/>
+      <AboutMe />
+      <Education />
+    </main>
+    );
   }
 }
 
